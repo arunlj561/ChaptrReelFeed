4ffa9f65ebc936d925198d7641e5162c69a08c92
arunjangidtvs <arun.jangid@tvsmotor.com>
2026-05-21 23:11:21 +0530

    Added Main file

---
+++ ChaptrReelFeed/ChaptrReelFeed/ChaptrReelFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaptrReelFeedApp.swift
+//  ChaptrReelFeed
+//
+//  Created by Arun Jangid on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ChaptrReelFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
