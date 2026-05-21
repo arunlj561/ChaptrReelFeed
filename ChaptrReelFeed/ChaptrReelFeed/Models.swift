da4782f33b8eb78c7b0758a38ca75d54df51612b
arunjangidtvs <arun.jangid@tvsmotor.com>
2026-05-21 23:13:33 +0530

    Added models for parsing Json

---
+++ ChaptrReelFeed/ChaptrReelFeed/Models.swift
@@ -0,0 +1,36 @@
+//
+//  VideoItem.swift
+//  ChaptrReelFeed
+//
+//  Created by Arun Jangid on 21/05/26.
+//
+
+
+import Foundation
+
+struct FeedResponse: Codable {
+
+    let videos: [VideoItem]
+
+}
+
+struct VideoItem: Codable, Identifiable {
+
+    let id: Int
+
+    let title: String
+
+    let duration: Int
+
+    let width: Int
+
+    let height: Int
+
+    let url: String
+
+    let thumbnail: String
+
+}
+struct VideoCatalog: Decodable, Sendable {
+    let videos: [VideoItem]
+}
